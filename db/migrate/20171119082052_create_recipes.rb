81d66c70fb08a0d62a3730166febb45fda89c811
TakefumiYamamura <yama1223xxx@gmail.com>
2017-11-20 02:26:58 +0900

    create db

--- db/migrate/20171119082052_create_recipes.rb
+++ db/migrate/20171119082052_create_recipes.rb
@@ -1,7 +1,11 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-
+      t.text :title, null: false
+      t.text :making_time, null: false
+      t.text :serves, null: false
+      t.text :ingredients, null: false
+      t.integer :cost, null: false
       t.timestamps null: false
     end
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+con = ActiveRecord::Base.connection
+con.execute("INSERT INTO recipes (id, title, making_time, serves, ingredients, cost, created_at, updated_at) VALUES (1, 'チキンカレー', '45分', '4人', '玉ねぎ,肉,スパイス', 1000, '2016-01-10 12:10:12', '2016-01-10 12:10:12');")
+con.execute("INSERT INTO recipes (id, title, making_time, serves, ingredients, cost, created_at, updated_at) VALUES (2, 'オムライス', '30分', '2人', '玉ねぎ,卵,スパイス,醤油', 700, '2016-01-11 13:10:12', '2016-01-11 13:10:12');")
